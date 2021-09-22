457f9349ec9d85a570f0058d5b056d8c76488c73
Amy Posten <amyposten85@gmail.com>
2021-09-22 08:39:15 -0400

    commit tree 0e7898158f7dc6ee93323ee1da62e87643bc1889

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const thankYou = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
